e8cf916713da79fa5d48c950aadefc5a5b0cb8c1
Shahedul Hoque <217447shah@gmsil.com>
2025-11-12 16:03:04 +0600

    added vercel.json

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
       const db = client.db('learningDB');    
       const learningCOllection = db.collection('courses');
@@ -123,7 +123,7 @@ async function run() {
    })
 
     
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
